0622c71f6f89b035e7e71d0bbcc5f5309eccd3f0
HarishS <38479086+harishgitu@users.noreply.github.com>
2022-07-08 14:59:10 -0400

    added pandas and streamlit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 st.title('My Parents New Healthy Diner')
 
@@ -9,3 +13,4 @@ st.markdown(':chicken: Hard-Boiled Free-Range Egg')
 st.markdown(':avocado::bread: Avacado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.dataframe(my_fruit_list)
