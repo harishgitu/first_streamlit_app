a64fbca48d5b0e85a96d9c5116e09230603de754
HarishS <38479086+harishgitu@users.noreply.github.com>
2022-07-08 14:31:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Favorites')
-st.markdown(':blueberries: Omega 3 & Blueberry Oatmeal')
-st.markdown(':leafy_green: Kale, Spinach & Rocket Smoothie')
+st.markdown(':grapes: Omega 3 & Blueberry Oatmeal')
+st.markdown(':cup_with_straw: Kale, Spinach & Rocket Smoothie')
 st.markdown(':chicken: Hard-Boiled Free-Range Egg')
 st.markdown(':avocado::bread: Avacado Toast')
